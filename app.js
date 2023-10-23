a8b3dd193c7509d2cb624a4cea3d3b0dfe5678c4
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-10-23 20:01:00 +0500

    added basic getRoute

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
+app.use("/api/v1/tasks", tasks);
+
 
 const port = process.env.PORT || 3100;
 
-app.get("/hello", (req, res)=>{
-    res.send("Task Manager App");
-})
+// app.get("/hello", (req, res)=>{
+//     res.send("Task Manager App");
+// })
+
+
 
 const start = ()=>{
     app.listen(port, console.log(`Server is listening at ${port}`))
